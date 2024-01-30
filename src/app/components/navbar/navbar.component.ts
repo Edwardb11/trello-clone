dcb67bf29abaa460382d44257def9db8c9b6bbd0
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2024-01-30 11:48:06 -0400

    Add FontAwesome icons to navbar component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  faBell,
+  faInfoCircle,
+  faClose,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
-  templateUrl: './navbar.component.html'
+  templateUrl: './navbar.component.html',
 })
 export class NavbarComponent implements OnInit {
   isOpen = false;
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  faBell = faBell;
+  faInfoCircle = faInfoCircle;
+  faClose = faClose;
+  constructor() {}
 
+  ngOnInit(): void {}
 }
