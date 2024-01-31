9282bab0bbfb407ed20d483f27d162693c6d879c
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2024-01-31 10:54:24 -0400

    Add ReactiveFormsModule and HttpClientModule imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { OverlayModule } from '@angular/cdk/overlay';
+import {CdkAccordionModule} from '@angular/cdk/accordion';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { DialogModule } from '@angular/cdk/dialog';
+import { CdkTableModule } from '@angular/cdk/table';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,18 +18,12 @@ import { BtnComponent } from './components/btn/btn.component';
 import { BoardsComponent } from './pages/boards/boards.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
-import { OverlayModule } from '@angular/cdk/overlay';
-import {CdkAccordionModule} from '@angular/cdk/accordion';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { DialogModule } from '@angular/cdk/dialog';
-import { CdkTableModule } from '@angular/cdk/table';
-
 import { BoardComponent } from './pages/board/board.component';
 import { TodoDialogComponent } from './components/todo-dialog/todo-dialog.component';
 import { ScrollComponent } from './pages/scroll/scroll.component';
-import { HttpClientModule } from '@angular/common/http';
 import { TableComponent } from './pages/table/table.component';
+
+
 
 @NgModule({
   declarations: [
@@ -42,7 +46,8 @@ import { TableComponent } from './pages/table/table.component';
     DragDropModule,
     DialogModule,
     HttpClientModule,
-    CdkTableModule
+    CdkTableModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
